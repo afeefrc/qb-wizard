fa0e112a66dca387a5dba04b1ab1b012501ad12f
afeefrc <afeef.rahman@gmail.com>
2024-09-22 23:25:09 +0530

    table setup for activity logs

--- src/renderer/components/trgInchargeTasks/ActivityLogs.tsx
+++ src/renderer/components/trgInchargeTasks/ActivityLogs.tsx
@@ -1,91 +1,105 @@
 import React from 'react';
-import { Tag, List } from 'antd';
-import { format } from 'date-fns';
+import { Tag, Table, Button, DatePicker } from 'antd';
+import type { TableColumnsType, TableProps } from 'antd';
+import { format, isWithinInterval } from 'date-fns';
 import { AppContext } from '../../context/AppContext';
-
-// interface ActivityLogsProps {
-//   logs: string[];
-// }
 
 function ActivityLogs() {
   const appContext = React.useContext(AppContext);
   const { userActivityLogs } = appContext || {};
 
-  // console.log(userActivityLogs);
-
   const targetTypeLabels = {
-    questionBank: <Tag color="geekblue">Question Bank</Tag>,
-    questionPaper: <Tag color="volcano">Question Paper</Tag>,
+    questionBank: (
+      <Tag color="geekblue">
+        <div style={{ fontSize: '14px' }}>Question Bank</div>
+      </Tag>
+    ),
+    questionPaper: (
+      <Tag color="volcano">
+        <div style={{ fontSize: '14px' }}>Question Paper</div>
+      </Tag>
+    ),
   };
+
+  const columns = [
+    {
+      title: 'Timestamp',
+      dataIndex: 'activityTime',
+      key: 'activityTime',
+      render: (activityTime: string) =>
+        format(new Date(activityTime), 'dd/MM/yyyy HH:mm:ss'),
+    },
+    {
+      title: 'Action',
+      dataIndex: 'action',
+      key: 'action',
+      width: '40%',
+      render: (action: string, record: any) => (
+        <>
+          <div style={{ fontWeight: 'bold', lineHeight: '2' }}>{action}</div>
+          <div style={{ lineHeight: '1.75' }}>{record.description}</div>
+        </>
+      ),
+      filters: Array.from(
+        new Set(userActivityLogs?.map((log) => log.unit) || []),
+      ).map((unit) => ({
+        text: unit,
+        value: unit,
+      })),
+      onFilter: (value: string, record: any) =>
+        record.unit.indexOf(value) === 0,
+    },
+    {
+      title: 'User',
+      dataIndex: 'user',
+      key: 'user',
+      render: (user: string, record: any) => (
+        <>
+          <div style={{ fontWeight: 'bold' }}>{user}</div>
+          {/* {record.members && (
+            <div
+              style={{ fontSize: '12px' }}
+            >{`Members: ${record.members}`}</div>
+          )} */}
+        </>
+      ),
+      filters: Array.from(
+        new Set(userActivityLogs?.map((log) => log.user) || []), // TODO: Trg incharge text case
+      ).map((user) => ({ text: user, value: user })),
+      onFilter: (value: string, record: any) =>
+        record.user.indexOf(value) === 0,
+    },
+    {
+      title: 'Task',
+      dataIndex: 'targetType',
+      key: 'targetType',
+      render: (targetType: keyof typeof targetTypeLabels) =>
+        targetTypeLabels[targetType] || targetType,
+      filters: Array.from(
+        new Set(userActivityLogs?.map((log) => log.targetType) || []),
+      ).map((targetType) => ({
+        text: targetType,
+        value: targetType,
+      })),
+      onFilter: (value: string, record: any) =>
+        record.targetType.indexOf(value) === 0,
+    },
+    {
+      title: 'Comment',
+      dataIndex: 'comment',
+      key: 'comment',
+      width: '20%',
+    },
+  ];
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center' }}>
-      <div style={{ width: '80%', margin: 0, padding: 10 }}>
-        {/* Custom header row */}
-        <div
-          style={{
-            display: 'flex',
-            fontWeight: 'bold',
-            marginBottom: '10px',
-            padding: '0 8px',
-          }}
-        >
-          <div style={{ width: '20%', padding: '0 5px' }}>Timestamp</div>
-          <div style={{ width: '40%', padding: '0 5px' }}>Action</div>
-          <div style={{ width: '15%', padding: '0 5px' }}>User</div>
-          <div style={{ width: '25%', padding: '0 5px' }}>Task</div>
-          <div style={{ width: '15%', padding: '0 5px' }}>Comment</div>
-        </div>
-
-        <List
-          itemLayout="horizontal"
+      <div style={{ width: '85%', margin: 0, padding: 10 }}>
+        <Table
+          columns={columns}
           dataSource={userActivityLogs}
-          pagination={false}
-          renderItem={(item: any) => (
-            <List.Item
-              style={{
-                display: 'flex',
-                padding: '8px',
-              }}
-            >
-              <div style={{ width: '20%', padding: '0 5px' }}>
-                <List.Item.Meta
-                  description={`${format(
-                    new Date(item.activityTime),
-                    'dd/MM/yyyy HH:mm:ss',
-                  )}`}
-                />
-              </div>
-              <div style={{ width: '40%', padding: '0 5px' }}>
-                <List.Item.Meta
-                  title={` ${item.action}`}
-                  description={`${item.description}`}
-                />
-              </div>
-              <div style={{ width: '15%', padding: '0 5px' }}>
-                <List.Item.Meta
-                  title={`${item.user}`}
-                  // description={
-                  //   item.members ? `Panel members: ${item.members}` : ''
-                  // }
-                />
-              </div>
-              <div style={{ width: '25%', padding: '0 5px' }}>
-                <List.Item.Meta
-                  description={
-                    targetTypeLabels[
-                      item.targetType as keyof typeof targetTypeLabels
-                    ] || item.targetType
-                  }
-                />
-              </div>
-              <div style={{ width: '15%', padding: '0 5px' }}>
-                {item.comment && (
-                  <List.Item.Meta description={`${item.comment}`} />
-                )}
-              </div>
-            </List.Item>
-          )}
+          rowKey={(record) => record.activityTime}
+          pagination={{ pageSize: 30 }}
         />
       </div>
     </div>
